7bee933adc742ff130caa05f0d2ef4bd3dbc774a
Andriy <puhan8@gmail.com>
2018-11-19 16:59:37 +0200

    1.0.0

---
+++ upload/src/addons/TG/UUA/XF/Service/User/Upgrade.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace TG\UUA\XF\Service\User;
+
+class Upgrade extends XFCP_Upgrade
+{
+	public function upgrade() 
+	{
+		if (!$this->finalSetup)
+		{
+			$this->finalSetup();
+		}
+
+		$active = $this->activeUpgrade;
+		$upgrade = $this->userUpgrade;
+		$user = $this->user;
+
+		if (!$active->user_upgrade_record_id)
+		{
+			if (!$upgrade->canPurchase() && !$this->ignoreUnpurchasable)
+			{
+				return false;
+			}
+		}
+
+		$db = $this->db();
+		$db->beginTransaction();
+
+		if (!$active->save(true, false))
+		{
+			$db->rollback();
+			return false;
+		}
+
+		/** @var UserGroupChange $userGroupChange */
+		$userGroupChange = $this->service('XF:User\UserGroupChange');
+		$userGroupChange->addUserGroupChange(
+			$user->user_id, 'userUpgrade-' . $upgrade->user_upgrade_id, $upgrade->extra_group_ids
+		);
+
+		/** @var \XF\Repository\UserAlert $alertRepo */
+		$alertRepo = $this->repository('XF:UserAlert');
+		$alertRepo->fastDeleteAlertsFromUser($user->user_id, 'user', $user->user_id, 'upgrade_end');
+
+        //$userIds = \XF::app()->options()->thDonate_user_notification;
+		
+
+		$alertUsers = $this->finder('XF:User')
+			->where('user_id', '=', \XF::app()->options()->tg_uua_users)->fetch();
+
+		foreach ($alertUsers as $alertUser) {
+			$extra = [
+				'link' => \XF::app()->router('public')->buildLink('members', $alertUser), 
+				'upgrade' => $upgrade->title,
+				'user' => $user->username
+			];
+
+			$alertRepo->alert($alertUser, $user->user_id, '', 'user', $user->user_id, 'tguua', $extra);
+			\XF::logError('all ok');
+		}
+		$db->commit();
+
+		return $active;
+	}
+}
